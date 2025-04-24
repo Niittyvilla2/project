cefdb9b3c2192fa00ee900c632784645c40e64b9
l3ksa <lauri.lehtinen7@metropolia.fi>
2025-04-24 11:43:24 +0300

    added reader

---
+++ reader.py
@@ -0,0 +1,20 @@
+from fifo import Fifo
+from machine import Pin
+from piotimer import Piotimer
+
+
+class Reader:
+    def __init__(self, pin):
+        self.fifo = Fifo(333, typecode='i')
+        self.data = Pin(pin, mode=Pin.IN)
+        self.timer = Piotimer(period=4, mode=Piotimer.PERIODIC, callback=self.poll_data)
+
+    def poll_data(self):
+        data = int(self.data.value())
+        self.fifo.put(data)
+
+    def read_next(self):
+        return self.fifo.get()
+
+    def has_data(self):
+        return self.fifo.has_data()
