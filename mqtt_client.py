40e7bffd7e372355f50061090477e58cf4f66a3f
HurttaM <Meeri.Hurtta@metropolia.fi>
2025-04-29 21:34:43 +0300

    Add files via upload
    mqqt_client

---
+++ mqtt_client.py
@@ -0,0 +1,30 @@
+from umqtt.simple import MQTTClient
+import json
+import time
+
+# MQTT settings
+MQTT_BROKER = "hrm02.asuscomm.com"
+MQTT_PORT = 1883
+TOPIC_RRI = "kubios-request"
+
+def upload_rr_intervals(rr_intervals):
+    try:
+        client = MQTTClient(client_id="hrva3000", server=MQTT_BROKER, port=MQTT_PORT)
+        client.connect()
+
+        payload = {
+            "id": int(time.time()),  # Unique ID from current time
+            "type": "RRI",
+            "data": rr_intervals,  # This should be a list of ints
+            "analysis": {
+                "type": "readiness"
+            }
+        }
+
+        client.publish(TOPIC_RRI, json.dumps(payload))
+        client.disconnect()
+        print("Data uploaded to Kubios Proxy.")
+
+    except Exception as e:
+        print("Upload failed:", e)
+
