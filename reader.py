5b5434f25ff392874a3a281b229c5b03aea2c2d0
l3ksa <lauri.lehtinen7@metropolia.fi>
2025-04-29 17:06:37 +0300

    reader fix

--- reader.py
+++ reader.py
@@ -7,6 +7,7 @@ class Reader:
     def __init__(self, pin):
         self.fifo = Fifo(333, typecode='i')
         self.data = Pin(pin, mode=Pin.IN)
+        self.tmr = None
         self.timer()
 
     def poll_data(self, tid):
@@ -19,4 +20,4 @@ class Reader:
         return self.fifo.has_data()
 
     def timer(self):
-        self.timer = Piotimer(period=10, mode=Piotimer.PERIODIC, callback=self.poll_data)
+        self.tmr = Piotimer(period=10, mode=Piotimer.PERIODIC, callback=self.poll_data)
